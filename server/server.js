f15643bd1278e9e88b9fe30691ec819e34eb98f0
moovex <igor@moovex.com>
2019-01-31 09:13:28 +0200

    add chmod

--- server/server.js
+++ server/server.js
@@ -3,19 +3,28 @@ const bodyParser = require('body-parser')
 const Sequelize = require("sequelize");
 const logger = require('morgan')
 const app = express();
+const dotenv=require('dotenv')
+dotenv.config()
 const port = process.env.PORT || 8082
 
 app.use(logger('combined'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use((req,res)=>{
+app.use((req,res,next)=>{
   res.status(404)
+  next(new Error("Not Found"))
+  
 })
 
 const http = require('http').Server(app)
-
-const dbConfig = require("./config/config.json").development;
+const dbConfig;
+if(process.env.NODE_ENV=='development'){
+  console.log("Dev Enviroment")
+   dbConfig = require("./config/config.json").development;
+}else{
+   dbConfig = require("./config/config.json").production;
+}
 const sequelize=new Sequelize(dbConfig.databse,dbConfig.username,dbConfig.password,{
   host: dbConfig.host,
   dialect: dbConfig.dialect,
