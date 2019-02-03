38d2b1c6ae90cf5216b4408b61953f286a961454
moovex <igor@moovex.com>
2019-02-03 09:08:01 +0200

    adding the eslint to project

--- server/server.js
+++ server/server.js
@@ -1,44 +1,51 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize")
 const logger = require('morgan')
-const app = express();
+const app = express()
 const dotenv=require('dotenv')
 dotenv.config()
 const port = process.env.PORT || 8082
+const devConfig=require("./config/config.json").development
+const prodConfig=require("./config/config.json").production
 
 app.use(logger('combined'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use((req,res,next)=>{
-  res.status(404)
-  next(new Error("Not Found"))
-});
+app.use((req, res, next)=>{
+    res.status(404)
+    next(new Error("Not Found"))
+})
 const http = require('http').Server(app)
-var dbConfig = {};
-if(process.env.NODE_ENV=='development'){
-  console.log("Dev Enviroment")
-   dbConfig = require("./config/config.json").development;
-}else{
-   dbConfig = require("./config/config.json").production;
+let dbConfig = {}
+if(process.env.NODE_ENV === 'development' ){
+    console.log("Dev Enviroment")
+    dbConfig = devConfig
 }
-const sequelize=new Sequelize(dbConfig.databse,dbConfig.username,dbConfig.password,{
-  host: dbConfig.host,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
-sequelize.authenticate().then(()=>{console.log("Connection establish successfully.")})
-.catch((error)=>{console.log(error,"Connection faild.")});
-http.listen(port,()=>{
-  console.log("Listening on port " + port)
-  app.emit('APP_START')
+else{
+    dbConfig = prodConfig
+}
+const sequelize=new Sequelize(dbConfig.databse, dbConfig.username, dbConfig.password, {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+})
+sequelize.authenticate().then(()=>{
+    console.log("Connection establish successfully.")
+})
+    .catch((error)=>{
+        console.log(error, "Connection faild.")
+    })
+http.listen(port, ()=>{
+    console.log("Listening on port " + port)
+    app.emit('APP_START')
 })
 module.exports=app
 
